d30f2b5250ba44e20762320dc2d474f0993c7b21
azankiew <checcol98@hotmail.it>
2022-04-05 17:30:30 +0200

    modifiche

--- Progetto/src/main/java/turcobardi/ALCReasoner.java
+++ Progetto/src/main/java/turcobardi/ALCReasoner.java
@@ -1,15 +1,33 @@
 package turcobardi;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLObject;
+import org.semanticweb.owlapi.model.OWLOntology;
+
 /*
  * 
  * This class is the ALC Reasoner and contains all the methods
  * for the reasoning
  */
 public class ALCReasoner {
-
+	
+	private OWLOntology concept = null;
+	
+	public ALCReasoner(OWLOntology concept) {
+		this.concept=concept;
+	}
 	/* rule to resolve intersection */
 	private void IntersectionRule() {
 		return;
+	}
+	
+	public void alcTableaux(int entity, Set<OWLObject> abox, Set<OWLObject> Lx) {
+		
+		
+		
 	}
 	
 }
--- Progetto/src/main/java/turcobardi/OntologyVisitor.java
+++ Progetto/src/main/java/turcobardi/OntologyVisitor.java
@@ -60,7 +60,7 @@ public class OntologyVisitor implements OWLObjectVisitor{
 	public void visit(OWLObjectComplementOf eq) {
 		out.print(not);
 		eq.getOperand().accept(this);
-		System.out.print(conceptToString(iri, eq.getOperand().toString()) + " ");
+		//System.out.print(conceptToString(iri, eq.getOperand().toString()) + " ");
 	}
 
 	public void visit(OWLEquivalentClassesAxiom eq) {
