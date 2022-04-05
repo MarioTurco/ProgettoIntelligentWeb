a61107a5ac3b820e728a458d44277f5820b3dd52
MarioTurco <mario.turco4@studenti.unina.it>
2022-04-05 17:30:37 +0200

    Ontology editor

---
+++ Progetto/src/main/java/turcobardi/OnotlogyEditor.java
@@ -0,0 +1,13 @@
+package turcobardi;
+
+import org.semanticweb.owlapi.model.OWLOntology;
+
+public class OnotlogyEditor {
+	OWLOntology ontology = null;
+	
+	public OnotlogyEditor(OWLOntology ontology) {
+		this.ontology = ontology;
+	}
+	
+	
+}
